97d9755dc5b65b8fb1911bebb988c9e33c6fc3ff
chuuungh1 <ouo168@naver.com>
2024-11-18 15:53:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write("지도test!!!")
+st.write("지도test111122")
 
 html_code = """
 <!DOCTYPE html>
@@ -66,9 +66,10 @@ html_code = """
     </div>
 </div>
 
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=393132b4dfde1b54fc18b3bacc06eb3f&libraries=services"></script>
+<script>
 
 
-<script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=393132b4dfde1b54fc18b3bacc06eb3f&libraries=services"></script>
 // 마커를 담을 배열입니다
 var markers = [];
 
