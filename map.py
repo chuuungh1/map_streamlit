9dbc678aaa2ed477fabd34327f1316c6883a9b72
chuuungh1 <ouo168@naver.com>
2024-11-20 00:55:16 +0900

    Update map.py

--- map.py
+++ map.py
@@ -10,16 +10,7 @@ html_code = """
     <meta charset="utf-8">
     <title>카카오 지도와 장소 검색</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=393132b4dfde1b54fc18b3bacc06eb3f&libraries=services"></script>
-    <script>
-function initKakaoMap() {
-    // 카카오 맵 초기화 코드
-    var map = new kakao.maps.Map(document.getElementById('map'), {
-        center: new kakao.maps.LatLng(37.5665, 126.978),
-        level: 3
-    });
-}
-</script>
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=393132b4dfde1b54fc18b3bacc06eb3f&libraries=services" async defer></script>
     <style>
         #map { width: 100%; height: 500px; }
         #placesList { list-style: none; padding: 0; }
@@ -34,96 +25,30 @@ function initKakaoMap() {
     <div id="map"></div>
 
     <script>
-        // 카카오 맵 API 로드 완료 후 초기화
-        kakao.maps.load(function() {
-            // 지도와 마커 관련 변수
-            var map;
-            var markers = [];
-
-            // 카카오 지도 초기화
-            function initMap() {
-                var mapContainer = document.getElementById('map');
-                var mapOption = {
-                    center: new kakao.maps.LatLng(37.5665, 126.978), // 서울 시청
+        // SDK 로드를 기다리는 함수
+        function initMap() {
+            if (typeof kakao !== 'undefined' && kakao.maps) {
+                var map = new kakao.maps.Map(document.getElementById('map'), {
+                    center: new kakao.maps.LatLng(37.5665, 126.978),
                     level: 3
-                };
-
-                map = new kakao.maps.Map(mapContainer, mapOption);
-            }
-
-            // 장소 검색 함수
-            function searchPlaces(lat, lon, radius, categoryCode) {
-                var apiKey = "6c1cbbc51f7ba2ed462ab5b62d3a3746";
-                var url = "https://dapi.kakao.com/v2/local/search/category.json";
-
-                $.ajax({
-                    method: "GET",
-                    url: url,
-                    data: {
-                        category_group_code: categoryCode,
-                        x: lon,
-                        y: lat,
-                        radius: radius
-                    },
-                    headers: { 
-                        Authorization: "KakaoAK " + apiKey 
-                    }
-                })
-                .done(function (msg) {
-                    displayPlaces(msg.documents);
-                })
-                .fail(function (jqXHR, textStatus, errorThrown) {
-                    console.error("API 호출 실패: " + textStatus, errorThrown);
                 });
+            } else {
+                console.log("Kakao Maps SDK not loaded yet");
             }
+        }
 
-            // 장소를 지도에 마커로 표시하는 함수
-            function displayPlaces(places) {
-                removeMarkers();
-                var placesList = $("#placesList");
-                placesList.empty();
-
-                places.forEach(function(place) {
-                    var position = new kakao.maps.LatLng(place.y, place.x);
-                    addMarker(position);
-                    placesList.append("<li>" + place.place_name + " - " + place.address_name + "</li>");
-                });
-            }
-
-            // 마커를 지도에 추가하는 함수
-            function addMarker(position) {
-                var marker = new kakao.maps.Marker({
-                    position: position
-                });
-                marker.setMap(map);
-                markers.push(marker);
-            }
-
-            // 이전 마커 제거
-            function removeMarkers() {
-                markers.forEach(function(marker) {
-                    marker.setMap(null);
-                });
-                markers = [];
-            }
-
-            // 버튼 클릭 시 장소 검색
-            $("#searchButton").click(function() {
-                var lat = 37.5665;
-                var lon = 126.978;
-                var radius = 1000;
-                var categoryCode = "FD6";
-
-                searchPlaces(lat, lon, radius, categoryCode);
-            });
-
-            // 페이지 로드 시 지도 초기화
-            initMap();
-        });
+        // SDK 로드 후 실행
+        window.onload = function() {
+            var checkKakaoLoaded = setInterval(function() {
+                if (typeof kakao !== 'undefined' && kakao.maps) {
+                    clearInterval(checkKakaoLoaded);
+                    initMap();
+                }
+            }, 100);
+        };
     </script>
 </body>
 </html>
-
 """
 
 # HTML 표시
