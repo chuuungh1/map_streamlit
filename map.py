6c59e1f7543962c4e8517de38ae7034733b48a3a
chuuungh1 <ouo168@naver.com>
2024-11-20 02:02:55 +0900

    Update map.py

--- map.py
+++ map.py
@@ -17,23 +17,6 @@ html_code = """
     </style>
 </head>
 <body>
-<div class="map_wrap">
-    <div id="map" style="width:100%;height:100%;position:relative;overflow:hidden;"></div>
-
-    <div id="menu_wrap" class="bg_white">
-        <div class="option">
-            <div>
-                <form onsubmit="searchPlaces(); return false;">
-                    키워드 : <input type="text" value="이태원 맛집" id="keyword" size="15"> 
-                    <button type="submit">검색하기</button> 
-                </form>
-            </div>
-        </div>
-        <hr>
-        <ul id="placesList"></ul>
-        <div id="pagination"></div>
-    </div>
-</div>
 
  <h1>카카오 지도와 장소 검색</h1>
     <input type="text" id="keyword" placeholder="검색할 장소 입력" />
